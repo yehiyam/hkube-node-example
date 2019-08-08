1a087ef1eb9ee72de0910734da7ec5bbbfefb763
Yehiyam Livneh <yehiyam@gmail.com>
2019-08-08 11:04:24 +0300

    fix

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const main = async () => {
             port: process.env.WORKER_SOCKET_PORT || 3000,
             host: process.env.WORKER_SOCKET_HOST || 'localhost',
             protocol: process.env.WORKER_SOCKET_PROTOCOL || 'ws',
-            url:'http://10.32.10.4/hkube/debug/foo'// process.env.WORKER_SOCKET_URL || null
+            url: process.env.WORKER_SOCKET_URL || null
+            // url:'http://10.32.10.4/hkube/debug/foo'
         }
     }
     nodejsWrapper = new NodejsWrapper();
